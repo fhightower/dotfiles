2befd0486267400c0ddab899a450c6766f6784ae
Floyd Hightower <floyd.hightower27@gmail.com>
2022-04-26 15:43:11 -0400

    Handling both https and ssh git paths

--- regularly_updated/.config/fish/functions/gcl.fish
+++ regularly_updated/.config/fish/functions/gcl.fish
@@ -1,4 +1,5 @@
-function gcl --wraps='git clone' --description 'alias gcl git clone'
-  set repo_name (echo $argv | cut -d "/" -f5 | cut -d "." -f1)
+function gcl
+  # hacky, but it works ;)
+  set repo_name (echo $argv | rev | cut -d "/" -f1 | rev | cut -d "." -f1)
   git clone $argv && cd $repo_name; 
 end
