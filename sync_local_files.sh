8a506815f175b327d723ee234577557bc424639d
Floyd Hightower <floyd.hightower27@gmail.com>
2022-07-29 04:58:43 -0400

    Add word file

--- sync_local_files.sh
+++ sync_local_files.sh
@@ -4,6 +4,7 @@ set BASE_CONFIG_PATH ~/.config
 set REG_UPDATE_PATH ./regularly_updated/.config
 
 cp $BASE_CONFIG_PATH/nvim/init.vim $REG_UPDATE_PATH/nvim/
+cp $BASE_CONFIG_PATH/nvim/spell/en.utf-8.add $REG_UPDATE_PATH/nvim/
 
 cp -r $BASE_CONFIG_PATH/fish/ $REG_UPDATE_PATH/fish
 
