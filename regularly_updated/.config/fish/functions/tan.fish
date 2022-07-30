1349c33d0626ea8e9d8df91b917fb50439c34e97
Floyd Hightower <floyd.hightower27@gmail.com>
2022-07-30 05:44:23 -0400

    Add more taskwarrior aliases

---
+++ regularly_updated/.config/fish/functions/tan.fish
@@ -0,0 +1,3 @@
+function tan --wraps='task annotate' --description 'alias tan task annotate'
+  task annotate $argv; 
+end
---
+++ regularly_updated/.config/fish/functions/td.fish
@@ -0,0 +1,3 @@
+function td --wraps='task done' --description 'alias td task done'
+  task done $argv; 
+end
