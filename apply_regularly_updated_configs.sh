7d49ae22c41bd23b0e23f49529b22d3c7408aa6a
Floyd <floyd.hightower27@gmail.com>
2022-11-04 16:49:45 -0400

    Use https repo link

--- apply_regularly_updated_configs.sh
+++ apply_regularly_updated_configs.sh
@@ -1,7 +1,7 @@
 # apply the regularly updated configs in this repo to a computer
 
 cd ~;
-git clone --depth=1 git@github.com:fhightower/dotfiles.git;
+git clone --depth=1 https://github.com/fhightower/dotfiles.git;
 cp -r dotfiles/regularly_updated/ ~;
 rm -rf dotfiles/
 cd -;
