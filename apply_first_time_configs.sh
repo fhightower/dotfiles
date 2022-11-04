22d87970c069c275974a61c8632ccb5b8f926f85
Floyd <floyd.hightower27@gmail.com>
2022-11-04 16:48:25 -0400

    Update to use https repo link

--- apply_first_time_configs.sh
+++ apply_first_time_configs.sh
@@ -1,7 +1,7 @@
 # apply the first time configs in this repo to a computer
 
 cd ~;
-git clone --depth=1 git@github.com:fhightower/dotfiles.git;
+git clone --depth=1 https://github.com/fhightower/dotfiles.git;
 cp -r dotfiles/first_time/ ~;
 rm -rf dotfiles/
 cd -;
