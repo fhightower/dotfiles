8750d024f0527d82ab8da29c925e61fdbf29197b
Floyd Hightower <floyd.hightower27@gmail.com>
2023-09-07 17:55:38 -0400

    Fix vertical preview :tada:

--- regularly_updated/.config/nvim/lua/general.lua
+++ regularly_updated/.config/nvim/lua/general.lua
@@ -5,3 +5,10 @@ require "dailynotes".setup({
     path = '~/notes/'
 })
 
+require('telescope').setup{
+    defaults = {
+        layout_strategy = 'vertical',
+        layout_config = { preview_cutoff = 0 },
+    },
+}
+
