4eb58621892b084b0e3163f616b110baad13729d
Floyd Hightower <floyd.hightower27@gmail.com>
2024-02-21 06:01:42 -0500

    Remove old token

--- regularly_updated/.config/fish/config.fish
+++ regularly_updated/.config/fish/config.fish
@@ -5,9 +5,6 @@ if status is-interactive
     fish_vi_key_bindings
 end
 
-# direnv hook fish | source
-export EXERCISM_TOKEN="1d44c66b-ed33-4a20-9d29-0e3f5c9421a4"
-
 set bashrc $HOME/.bashrc
 [ -f "$bashrc" ] && bash "$bashrc"
 
